97afc2c5a3bbd51807be7dd5fd052c537d12088d
alexb <alexb@DESKTOP-555UBVN>
2025-04-08 21:07:34 +0300

    Your commit message

--- Contact Manager Graphical/Form1.Designer.cs
+++ Contact Manager Graphical/Form1.Designer.cs
@@ -28,8 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            tabControl1 = new TabControl();
-            tabPage1 = new TabPage();
             label3 = new Label();
             label2 = new Label();
             listBox2 = new ListBox();
@@ -38,78 +36,50 @@
             button1 = new Button();
             textBox1 = new TextBox();
             listBox1 = new ListBox();
-            tabPage2 = new TabPage();
-            tabPage3 = new TabPage();
-            tabPage4 = new TabPage();
-            tabControl1.SuspendLayout();
-            tabPage1.SuspendLayout();
+            menuStrip1 = new MenuStrip();
+            editToolStripMenuItem = new ToolStripMenuItem();
+            exitescToolStripMenuItem = new ToolStripMenuItem();
+            exportContactsToTxtToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
-            // 
-            // tabControl1
-            // 
-            tabControl1.Controls.Add(tabPage1);
-            tabControl1.Controls.Add(tabPage2);
-            tabControl1.Controls.Add(tabPage3);
-            tabControl1.Controls.Add(tabPage4);
-            tabControl1.Font = new Font("Segoe UI", 15F);
-            tabControl1.Location = new Point(2, 0);
-            tabControl1.Name = "tabControl1";
-            tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(796, 623);
-            tabControl1.TabIndex = 0;
-            // 
-            // tabPage1
-            // 
-            tabPage1.Controls.Add(label3);
-            tabPage1.Controls.Add(label2);
-            tabPage1.Controls.Add(listBox2);
-            tabPage1.Controls.Add(button2);
-            tabPage1.Controls.Add(label1);
-            tabPage1.Controls.Add(button1);
-            tabPage1.Controls.Add(textBox1);
-            tabPage1.Controls.Add(listBox1);
-            tabPage1.Location = new Point(4, 37);
-            tabPage1.Name = "tabPage1";
-            tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(788, 582);
-            tabPage1.TabIndex = 0;
-            tabPage1.Text = "All Contacts";
-            tabPage1.UseVisualStyleBackColor = true;
-            tabPage1.Click += tabPage1_Click;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(360, 133);
+            label3.Font = new Font("Segoe UI", 15F);
+            label3.Location = new Point(363, 169);
             label3.Name = "label3";
             label3.Size = new Size(123, 28);
-            label3.TabIndex = 7;
+            label3.TabIndex = 15;
             label3.Text = "Contact Info:";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(33, 133);
+            label2.Font = new Font("Segoe UI", 15F);
+            label2.Location = new Point(36, 169);
             label2.Name = "label2";
             label2.Size = new Size(120, 28);
-            label2.TabIndex = 6;
+            label2.TabIndex = 14;
             label2.Text = "All Contacts:";
             // 
             // listBox2
             // 
+            listBox2.Font = new Font("Segoe UI", 15F);
             listBox2.FormattingEnabled = true;
             listBox2.ItemHeight = 28;
-            listBox2.Location = new Point(360, 164);
+            listBox2.Location = new Point(363, 200);
             listBox2.Name = "listBox2";
             listBox2.Size = new Size(393, 172);
-            listBox2.TabIndex = 5;
+            listBox2.TabIndex = 13;
             // 
             // button2
             // 
-            button2.Location = new Point(494, 60);
+            button2.Font = new Font("Segoe UI", 15F);
+            button2.Location = new Point(497, 96);
             button2.Name = "button2";
             button2.Size = new Size(156, 38);
-            button2.TabIndex = 4;
+            button2.TabIndex = 12;
             button2.Text = "ALL CONTACTS";
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
@@ -117,98 +87,118 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(33, 29);
+            label1.Font = new Font("Segoe UI", 15F);
+            label1.Location = new Point(36, 65);
             label1.Name = "label1";
             label1.Size = new Size(266, 28);
-            label1.TabIndex = 3;
+            label1.TabIndex = 11;
             label1.Text = "Search By Name OR Number:";
             // 
             // button1
             // 
-            button1.Location = new Point(359, 60);
+            button1.Font = new Font("Segoe UI", 15F);
+            button1.Location = new Point(362, 96);
             button1.Name = "button1";
             button1.Size = new Size(93, 38);
-            button1.TabIndex = 2;
+            button1.TabIndex = 10;
             button1.Text = "SEARCH";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(33, 60);
+            textBox1.Font = new Font("Segoe UI", 15F);
+            textBox1.Location = new Point(36, 96);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(288, 34);
-            textBox1.TabIndex = 1;
-            textBox1.TextChanged += textBox1_TextChanged;
+            textBox1.TabIndex = 9;
             // 
             // listBox1
             // 
+            listBox1.Font = new Font("Segoe UI", 15F);
             listBox1.FormattingEnabled = true;
             listBox1.ItemHeight = 28;
-            listBox1.Location = new Point(33, 164);
+            listBox1.Location = new Point(36, 200);
             listBox1.Name = "listBox1";
             listBox1.Size = new Size(288, 284);
-            listBox1.TabIndex = 0;
+            listBox1.TabIndex = 8;
+            listBox1.SelectedIndexChanged += listBoxContacts_SelectedIndexChanged;
             // 
-            // tabPage2
+            // menuStrip1
             // 
-            tabPage2.Location = new Point(4, 37);
-            tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(788, 582);
-            tabPage2.TabIndex = 1;
-            tabPage2.Text = "Create Contact";
-            tabPage2.UseVisualStyleBackColor = true;
+            menuStrip1.BackColor = SystemColors.AppWorkspace;
+            menuStrip1.Font = new Font("Segoe UI", 15F);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { editToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(792, 36);
+            menuStrip1.TabIndex = 16;
+            menuStrip1.Text = "menuStrip1";
             // 
-            // tabPage3
+            // editToolStripMenuItem
             // 
-            tabPage3.Location = new Point(4, 37);
-            tabPage3.Name = "tabPage3";
-            tabPage3.Padding = new Padding(3);
-            tabPage3.Size = new Size(788, 582);
-            tabPage3.TabIndex = 2;
-            tabPage3.Text = "Delete Contact";
-            tabPage3.UseVisualStyleBackColor = true;
+            editToolStripMenuItem.BackColor = Color.LightGray;
+            editToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { exitescToolStripMenuItem, exportContactsToTxtToolStripMenuItem });
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new Size(54, 32);
+            editToolStripMenuItem.Text = "File";
+            editToolStripMenuItem.Click += editToolStripMenuItem_Click;
             // 
-            // tabPage4
+            // exitescToolStripMenuItem
             // 
-            tabPage4.Location = new Point(4, 37);
-            tabPage4.Name = "tabPage4";
-            tabPage4.Padding = new Padding(3);
-            tabPage4.Size = new Size(788, 582);
-            tabPage4.TabIndex = 3;
-            tabPage4.Text = "Update Contact";
-            tabPage4.UseVisualStyleBackColor = true;
+            exitescToolStripMenuItem.BackColor = Color.LightGray;
+            exitescToolStripMenuItem.Font = new Font("Segoe UI", 13F);
+            exitescToolStripMenuItem.Name = "exitescToolStripMenuItem";
+            exitescToolStripMenuItem.Size = new Size(257, 30);
+            exitescToolStripMenuItem.Text = "Exit (esc)";
+            exitescToolStripMenuItem.Click += exitescToolStripMenuItem_Click;
+            // 
+            // exportContactsToTxtToolStripMenuItem
+            // 
+            exportContactsToTxtToolStripMenuItem.BackColor = Color.LightGray;
+            exportContactsToTxtToolStripMenuItem.Font = new Font("Segoe UI", 13F);
+            exportContactsToTxtToolStripMenuItem.Name = "exportContactsToTxtToolStripMenuItem";
+            exportContactsToTxtToolStripMenuItem.Size = new Size(257, 30);
+            exportContactsToTxtToolStripMenuItem.Text = "Export contacts to .txt";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(792, 539);
-            Controls.Add(tabControl1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(listBox2);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
-            tabControl1.ResumeLayout(false);
-            tabPage1.ResumeLayout(false);
-            tabPage1.PerformLayout();
+            KeyDown += Form1_KeyDown;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private TabControl tabControl1;
-        private TabPage tabPage1;
-        private TabPage tabPage2;
-        private TextBox textBox1;
-        private ListBox listBox1;
-        private TabPage tabPage3;
-        private TabPage tabPage4;
-        private Button button1;
-        private Label label1;
-        private Button button2;
         private Label label3;
         private Label label2;
         private ListBox listBox2;
+        private Button button2;
+        private Label label1;
+        private Button button1;
+        private TextBox textBox1;
+        private ListBox listBox1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem editToolStripMenuItem;
+        private ToolStripMenuItem exitescToolStripMenuItem;
+        private ToolStripMenuItem exportContactsToTxtToolStripMenuItem;
     }
 }
--- Contact Manager Graphical/Form1.cs
+++ Contact Manager Graphical/Form1.cs
@@ -1,6 +1,7 @@
 using Contact_Manager_Graphical.Models;
 using Microsoft.EntityFrameworkCore;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using System.Windows.Forms;
 
 namespace Contact_Manager_Graphical
 {
@@ -30,14 +31,49 @@ namespace Contact_Manager_Graphical
         }
         private void Form1_Load(object sender, EventArgs e)
         {
+            this.KeyPreview = true;
+
             using (var context = new ContactmanagerContext())
             {
                 AllContacts();
             }
         }
+        private void listBoxContacts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            var selectedContact = listBox1.SelectedItem.ToString();
 
+            using (var context = new ContactmanagerContext())
+            {
+                string[] nameParts = selectedContact.Split('|')[0].Trim().Split(' ');
+                string firstName = nameParts[0];
+                string secondName = nameParts[1];
+
+                var person = context.People
+                    .Include(p => p.Contacts)
+                    .FirstOrDefault(p => p.FirstName == firstName && p.SecondName == secondName);
+
+                if (person != null)
+                {
+                    foreach (var contact in person.Contacts)
+                    {
+                        listBox2.Items.Add($"Phone: {contact.PhoneNum}");
+                        listBox2.Items.Add($"Email: {contact.Email}");
+                        listBox2.Items.Add($"Address: {person.Address}");
+                        listBox2.Items.Add($"Birth date: {person.BirthDate}");
+                        listBox2.Items.Add("---------------------------------------------------");
+                        listBox2.Items.Add($"Contact Created: {contact.CreationDate}");
+                    }
+                }
+            }
+        }
         private void button1_Click(object sender, EventArgs e)
         {
+            if (listBox1.SelectedItem != null)
+            {
+                listBox1.Items.Clear();
+                listBox2.Items.Clear();
+            }
             listBox1.Items.Clear();
             string searchTerm = textBox1.Text.ToLower();
 
@@ -71,7 +107,29 @@ namespace Contact_Manager_Graphical
 
             }
         }
-        private void tabPage1_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            using (var context = new ContactmanagerContext())
+            {
+                AllContacts();
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+        private void exitescToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    
+    private void tabPage1_Click(object sender, EventArgs e)
         {
 
         }
@@ -81,14 +139,12 @@ namespace Contact_Manager_Graphical
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            textBox1.Text = string.Empty;
-            listBox1.Items.Clear();
-            using (var context = new ContactmanagerContext())
-            {
-                AllContacts();
-            }
+
         }
+
     }
 }
